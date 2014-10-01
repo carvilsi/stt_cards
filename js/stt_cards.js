3ad66061ddc33b7006f9265b7a24df4169df6dc1
carvilsi <carvilsi.mail.list@gmail.com>
2014-10-01 06:45:40 +0200

    ver 0.1

--- js/stt_cards.js
+++ js/stt_cards.js
@@ -22,28 +22,31 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+var Stt_cards = {
+    
+    version: '0.1',
    
     /**
     Como siempre algunas variables útiles
     */
     
-    var recognition;
-    var camp = [];
-    var numeroFichas = 0;
-    var sepCSV = ";";
-    var enter = '\n';
+    recognition: '',
+    camp: [],
+    numeroFichas: 0,
+    sepCSV: ";",
+    enter: '\n',
     
-    var longArray = [];
+    longArray: [],
     
     /**
     Exporta los datos introducidos a un archivo csv para descargar
     */
     
-    function exportarDatos() {
-        
-        var csv = camp.join(";") + enter;
-        console.log(localStorage.length);
-        for (var i=0; i<localStorage.length; i++) {
+    exportarDatos: function () {
+        'use strict';
+        var i = 0;
+        var csv = Stt_cards.camp.join(";") + Stt_cards.enter;
+        for (i; i < localStorage.length; i++) {
             csv = csv + localStorage.getItem(i);
         }
         var blob = new Blob([csv], {type: "text/csv"});
@@ -53,58 +56,66 @@ THE SOFTWARE.
         var date = new Date();
         a.download = 'tuFicha_' + date + '.csv';  
         localStorage.clear();
-    }
+    },
     
     /**
     Mete los datos en el localStorage
     */
     
-    function datosOk() {
+    datosOk: function () {
             var datos = "";
-            camp.forEach(function (campo, index){
-                datos += $("input[name='" + campo +"']").val() + sepCSV;
+            Stt_cards.camp.forEach(function (campo, index){
+                datos += $("input[name='" + campo +"']").val();
             });
-            datos += enter;
-            localStorage.setItem(numeroFichas,datos);
-            numeroFichas += 1;
-            muestraOculta(false);
-            if ( numeroFichas >= 1) {
+            if (!datos) {
+                return;
+            }
+            datos = "";
+            Stt_cards.camp.forEach(function (campo, index){
+                datos += $("input[name='" + campo +"']").val() + Stt_cards.sepCSV;
+            });
+            datos += Stt_cards.enter;
+            localStorage.setItem(this.numeroFichas,datos);
+            Stt_cards.numeroFichas += 1;
+            this.muestraOculta(false);
+            if ( this.numeroFichas >= 1) {
               $('#csvDownload').css('display','inline');  
             }
             $('#empezar').css('display','inline');  
-       }
+       },
     
     /**
     ejecutamos al iniciar
     */
     
-    function init() {
-        muestraOculta(false);
+    init: function () {
+        setTimeout(function () {
+            Stt_cards.muestraOculta(false);
         $('#parar').css('display','none');
         $('#empezar').css('display','none');
-    }
+        }, 100);
+    },
     
     /**
     para empezar con la cosa de las transcripción
     */
      
-    function grabar() {
+    grabar: function () {
         if (!('webkitSpeechRecognition' in window)) {
-            noVaLaCosa();
+            this.noVaLaCosa();
         } else {
             $('#parar').css('display','inline');
             $('#empezar').css('display','none');
-            var out = $('#textarea');
-            recognition = new webkitSpeechRecognition();
-            recognition.lang = "es-ES";
-            recognition.continuous = true;
+            this.recognition = new webkitSpeechRecognition();
+            this.recognition.lang = "es-ES";
+            this.recognition.continuous = true;
             var final_transcript = '';
             var match;
-            recognition.onresult = function(event) {
+            this.recognition.onresult = function(event) {
                 var interim_transcript = '';
-                if (typeof(event.results) == 'undefined') {
-                    recognition.onend = null;
-                    recognition.stop();
+                if (typeof (event.results) == 'undefined') {
+                    this.recognition.onend = null;
+                    this.recognition.stop();
                     return;
                 }
                 for (var i = event.resultIndex; i < event.results.length; ++i) {
@@ -114,17 +125,17 @@ THE SOFTWARE.
                         var transArray = transcript.split(" ");
                         if (transArray.length > 1) {
                             var tmparry = transArray.slice(0,2);
-                            var find = $.inArray(tmparry.join(" "),camp);
+                            var find = $.inArray(tmparry.join(" "),Stt_cards.camp);
                             if (find != -1) {
-                                $("input[name='" + camp[find] +"']").focus();
+                                $("input[name='" + this.camp[find] +"']").focus();
                                 var t = transArray.slice(2,transArray.length);
-                                $("input[name='" + camp[find] +"']").val(t.join(" "));
+                                $("input[name='" + this.camp[find] +"']").val(t.join(" "));
                             } else {
-                                find = $.inArray(transArray[0],camp);
+                                find = $.inArray(transArray[0],Stt_cards.camp);
                                 if (find != -1) {
-                                    $('#' + camp[find]).focus();
+                                    $('#' + Stt_cards.camp[find]).focus();
                                     var t = transArray.slice(1,transArray.length);
-                                    $('#' + camp[find]).val(t.join(" "));
+                                    $('#' + Stt_cards.camp[find]).val(t.join(" "));
                                 } else {
                                     console.log('nada :/');
                                 }
@@ -138,44 +149,44 @@ THE SOFTWARE.
             
             // Empezamos con la cosa del escuchar para transcribir
             
-            recognition.start();
+            this.recognition.start();
             
             // Manejar esa clase de cosas relacionadas con la transcripción
             
-            recognition.onstart = function () {
+            this.recognition.onstart = function () {
                 $('#escuchando').css('display','inline');
             }
             
-            recognition.onstop = function () {
+            this.recognition.onstop = function () {
                 $('#escuchando').css('display','none');
             }
         } 
-    }
+    },
     
     /**
     Para cuando paramos el dictado
     */
     
-        function stopstt() {
-            recognition.stop(); 
+        stopstt: function () {
+            this.recognition.stop(); 
             $('#parar').css('display','none');
             $('#empezar').css('display','none');
             $('#datosOk').css('display','inline');
             $('#escuchando').css('display','none');
-        }
+        },
     
     /**
     Cuando empezamos el dictado
     */
     
     
-        function startstt() {
-            muestraOculta(false);
-            camp.forEach(function (campo, index){
+        startstt: function () {
+            this.muestraOculta(false);
+            this.camp.forEach(function (campo, index){
                     $("input[name='" + campo +"']").val('');
             });
-            grabar();
-        }
+            this.grabar();
+        },
     
     /**
     El explorador web no soporta el dictado, y aunque dugan ortas cosas, por ahí, solo lo soporta Google Chrome
@@ -185,18 +196,18 @@ THE SOFTWARE.
     */
     
         
-        function noVaLaCosa() {
+        noVaLaCosa: function () {
             $('#nova').css('display','inline');
             $('#stop').css('display','none');        
             $('#start').css('display','none');        
-        }
+        },
     
     
     /**
     mostar u ocultar algunos botones "a pajera abierta"
     */
         
-        function muestraOculta (bool) {
+        muestraOculta: function (bool) {
             if (bool) {
                 $('#datosOk').css('display','inline');
                 $('#csvDownload').css('display','inline');
@@ -204,33 +215,32 @@ THE SOFTWARE.
                 $('#datosOk').css('display','none');
                 $('#csvDownload').css('display','none');
             }
-        }
+        },
     
     /**
     Tomar los campos del cuadro de texto y crear los input
     */
     
-    function getCampos() {
+    getCampos: function () {
         var campos = $('#campos').val();
         if (campos) {
             var ctmp = campos.split("\n");
-            ctmp.forEach(function(campo){
+            ctmp.forEach(function(campo, index){
                 var c = campo.trim();
                 if (c) {
-                    camp.push(c);
+                    Stt_cards.camp.push(c);
                     $('#otro').append('<div style="width:98%" class="ui-field-contain"><label for="' + c + '">' + c + ':' + '</label><input data-clear-btn="true"  name="' + c + '" id="' + c + '" placeholder="" value="" type="text"></div>');
                     }
                 });
             $('#otro').trigger("create");
             $('#empezar').css('display','inline');
             $('#getCampos').css('display','none');
+        } else {
+            return;
         }
         $('#texto').css('display','none');
         
     }
-      
+};      
     
-    
-    window.addEventListener("load", init, false);  
-
-
+Stt_cards.init();
