5795bbf708e20a941659b33e129f2df69f8efb63
carvilsi <carvilsi.mail.list@gmail.com>
2014-10-03 02:19:22 +0200

    cambio en js y html

--- js/stt_cards.js
+++ js/stt_cards.js
@@ -23,95 +23,101 @@ THE SOFTWARE.
 */
 
 var Stt_cards = {
-    
+
     version: '0.1',
-   
+
     /**
     Como siempre algunas variables útiles
     */
-    
+
     recognition: '',
     camp: [],
     numeroFichas: 0,
     sepCSV: ";",
     enter: '\n',
-    
     longArray: [],
-    
+
+    elemIn: '',
+    divOut: '',
+    listen: false,
+
+    /**
+    Inicializamos estos dos elementos, un input para tomar los campos y un div para crear los inputs
+    */
+
+    init: function (elemInput, divOutput) {
+        'use strict';
+        try {
+            Stt_cards.elemIn = elemInput;
+            Stt_cards.divOut = divOutput;
+            return true;
+        } catch (e) {
+            console.log('error :/ ' + e);
+            return false;
+        }
+    },
+
+
     /**
     Exporta los datos introducidos a un archivo csv para descargar
     */
-    
-    exportarDatos: function () {
+    exportarDatos: function (button) {
         'use strict';
         var i = 0;
         var csv = Stt_cards.camp.join(";") + Stt_cards.enter;
         for (i; i < localStorage.length; i++) {
             csv = csv + localStorage.getItem(i);
         }
-        var blob = new Blob([csv], {type: "text/csv"});
+        var blob = new Blob([csv], {
+            type: "text/csv"
+        });
         var url = URL.createObjectURL(blob);
-        var a = document.querySelector("#csvDownload"); // id of the <a> element to render the download link
+        var a = document.querySelector(button);
         a.href = url;
         var date = new Date();
-        a.download = 'tuFicha_' + date + '.csv';  
+        a.download = 'tuFicha_' + date + '.csv';
         localStorage.clear();
     },
-    
     /**
-    Mete los datos en el localStorage
+    @TODO: Mete los datos en el localStorage
     */
-    
+
     datosOk: function () {
+        try {
             var datos = "";
-            Stt_cards.camp.forEach(function (campo, index){
-                datos += $("input[name='" + campo +"']").val();
+            Stt_cards.camp.forEach(function (campo, index) {
+                datos += $("input[name='" + campo + "']").val();
             });
             if (!datos) {
                 return;
             }
             datos = "";
-            Stt_cards.camp.forEach(function (campo, index){
-                datos += $("input[name='" + campo +"']").val() + Stt_cards.sepCSV;
+            Stt_cards.camp.forEach(function (campo, index) {
+                datos += $("input[name='" + campo + "']").val() + Stt_cards.sepCSV;
             });
             datos += Stt_cards.enter;
-            localStorage.setItem(this.numeroFichas,datos);
+            localStorage.setItem(this.numeroFichas, datos);
             Stt_cards.numeroFichas += 1;
-            this.muestraOculta(false);
-            if ( this.numeroFichas >= 1) {
-              $('#csvDownload').css('display','inline');  
-            }
-            $('#empezar').css('display','inline');  
-       },
-    
-    /**
-    ejecutamos al iniciar
-    */
-    
-    init: function () {
-        setTimeout(function () {
-            Stt_cards.muestraOculta(false);
-        $('#parar').css('display','none');
-        $('#empezar').css('display','none');
-        }, 100);
+            return true;
+        } catch (e) {
+            return false;
+        }
     },
-    
+
     /**
     para empezar con la cosa de las transcripción
     */
-     
+
     grabar: function () {
         if (!('webkitSpeechRecognition' in window)) {
-            this.noVaLaCosa();
+            this.noVaLaCosa(); // TODO: será necesario injectarlo en el DOM
         } else {
-            $('#parar').css('display','inline');
-            $('#empezar').css('display','none');
             this.recognition = new webkitSpeechRecognition();
-            this.recognition.lang = "es-ES";
+            this.recognition.lang = "es-ES"; // seto molaría que fuera opcional
             this.recognition.continuous = true;
             var final_transcript = '';
             var match;
-            this.recognition.onresult = function(event) {
+            this.recognition.onresult = function (event) {
                 var interim_transcript = '';
                 if (typeof (event.results) == 'undefined') {
                     this.recognition.onend = null;
@@ -121,128 +127,116 @@ var Stt_cards = {
                 for (var i = event.resultIndex; i < event.results.length; ++i) {
                     if (event.results[i].isFinal) {
                         var transcript = event.results[event.resultIndex][0].transcript;
-                        transcript = transcript.trim();       
+                        transcript = transcript.trim();
                         var transArray = transcript.split(" ");
                         if (transArray.length > 1) {
-                            var tmparry = transArray.slice(0,2);
-                            var find = $.inArray(tmparry.join(" "),Stt_cards.camp);
+                            var tmparry = transArray.slice(0, 2);
+                            var find = $.inArray(tmparry.join(" "), Stt_cards.camp);
                             if (find != -1) {
-                                $("input[name='" + this.camp[find] +"']").focus();
-                                var t = transArray.slice(2,transArray.length);
-                                $("input[name='" + this.camp[find] +"']").val(t.join(" "));
+                                $("input[name='" + this.camp[find] + "']").focus();
+                                var t = transArray.slice(2, transArray.length);
+                                $("input[name='" + this.camp[find] + "']").val(t.join(" "));
                             } else {
-                                find = $.inArray(transArray[0],Stt_cards.camp);
+                                find = $.inArray(transArray[0], Stt_cards.camp);
                                 if (find != -1) {
                                     $('#' + Stt_cards.camp[find]).focus();
-                                    var t = transArray.slice(1,transArray.length);
+                                    var t = transArray.slice(1, transArray.length);
                                     $('#' + Stt_cards.camp[find]).val(t.join(" "));
                                 } else {
                                     console.log('nada :/');
                                 }
-                            }    
+                            }
                         } else {
                             console.log('nada :/');
                         }
                     }
                 }
             };
-            
+
             // Empezamos con la cosa del escuchar para transcribir
-            
+
             this.recognition.start();
-            
+
             // Manejar esa clase de cosas relacionadas con la transcripción
-            
+
             this.recognition.onstart = function () {
-                $('#escuchando').css('display','inline');
+                Stt_cards.listen = true;
             }
-            
+
             this.recognition.onstop = function () {
-                $('#escuchando').css('display','none');
+                Stt_cards.listen = false;
             }
-        } 
+        }
     },
-    
+
+
     /**
     Para cuando paramos el dictado
     */
-    
-        stopstt: function () {
-            this.recognition.stop(); 
-            $('#parar').css('display','none');
-            $('#empezar').css('display','none');
-            $('#datosOk').css('display','inline');
-            $('#escuchando').css('display','none');
-        },
-    
+
+    stopstt: function () {
+        this.recognition.stop();
+    },
+
     /**
     Cuando empezamos el dictado
     */
-    
-    
-        startstt: function () {
-            this.muestraOculta(false);
-            this.camp.forEach(function (campo, index){
-                    $("input[name='" + campo +"']").val('');
-            });
-            this.grabar();
-        },
-    
+
+
+    startstt: function () {
+        this.camp.forEach(function (campo, index) {
+            $("input[name='" + campo + "']").val('');
+        });
+        this.grabar();
+    },
+
     /**
-    El explorador web no soporta el dictado, y aunque dugan ortas cosas, por ahí, solo lo soporta Google Chrome
+    El explorador web no soporta el dictado, y aunque digan ortas cosas, por ahí, solo lo soporta Google Chrome
     Mostramos un aviso al usuario 
-    
+
     TODO: meter una comprobación al principio para evitar mal entendidos :P
     */
-    
-        
-        noVaLaCosa: function () {
-            $('#nova').css('display','inline');
-            $('#stop').css('display','none');        
-            $('#start').css('display','none');        
-        },
-    
-    
-    /**
-    mostar u ocultar algunos botones "a pajera abierta"
-    */
-        
-        muestraOculta: function (bool) {
-            if (bool) {
-                $('#datosOk').css('display','inline');
-                $('#csvDownload').css('display','inline');
-            } else {
-                $('#datosOk').css('display','none');
-                $('#csvDownload').css('display','none');
-            }
-        },
-    
-    /**
-    Tomar los campos del cuadro de texto y crear los input
-    */
-    
-    getCampos: function () {
-        var campos = $('#campos').val();
-        if (campos) {
-            var ctmp = campos.split("\n");
-            ctmp.forEach(function(campo, index){
-                var c = campo.trim();
-                if (c) {
-                    Stt_cards.camp.push(c);
-                    $('#otro').append('<div style="width:98%" class="ui-field-contain"><label for="' + c +
-                     '">' + c + ':' + '</label><input data-clear-btn="false"  name="' + c + '" id="' + c +
-                      '" placeholder="" value="" type="text"></div>');
-                    }
-                });
-            $('#otro').trigger("create");
-            $('#empezar').css('display','inline');
-            $('#getCampos').css('display','none');
+
+
+    noVaLaCosa: function () {
+        if (Stt_cards.divOut) {
+            $(Stt_cards.divOut).append('<br/><br/><p id="nova" style="display:none">Web Speech API no rula en este explorador web. Usa mejor <a href="//www.google.com/chrome">Chrome</a> versión 25 o superior.</p>');
         } else {
             return;
         }
-        $('#texto').css('display','none');
-        
+    },
+
+
+    /**
+    Tomar los campos del cuadro de texto y crear los input
+    */
+
+    getCampos: function () {
+        if (Stt_cards.elemIn && Stt_cards.divOut) {
+            var campos = $(Stt_cards.elemIn).val();
+            if (campos) {
+                var ctmp = campos.split("\n");
+                ctmp.forEach(function (campo, index) {
+                    var c = campo.trim();
+                    if (c) {
+                        Stt_cards.camp.push(c);
+                        $(Stt_cards.divOut).append('<div style="width:98%" class="ui-field-contain"><label for="' + c +
+                            '">' + c + ':' + '</label><input data-clear-btn="false"  name="' + c + '" id="' + c +
+                            '" placeholder="" value="" type="text"></div>');
+                    }
+                });
+                $(Stt_cards.divOut).trigger("create");
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+            console.log('need to init Stt_cards()');
+        }
     }
-};      
-    
+
+
+};
+
 Stt_cards.init();
